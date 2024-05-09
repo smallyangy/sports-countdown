160ba4c2b87e000dc188558ab6bddd6c8faf30ec
smallyangy <315862477@qq.com>
2024-05-09 10:45:21 +0800

    finish

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    base: './',
     server: {
         host: '0.0.0.0',
         port: 8808,
